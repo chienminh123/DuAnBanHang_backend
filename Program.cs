e02f44d5e286ef66c23d810a5424afc5a9f834e0
chienminh123 <chien08042k4@gmail.com>
2026-05-18 14:30:41 +0700

    sửa program

--- Program.cs
+++ Program.cs
@@ -47,12 +47,13 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
     app.UseSwagger();
     app.UseSwaggerUI();
-}
-
 app.UseCors("AllowReact");
 app.UseStaticFiles();
 app.UseAuthentication();
